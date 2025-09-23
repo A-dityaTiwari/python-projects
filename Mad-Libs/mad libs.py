56d58505566e58eca25b5dc24609f3a820482799
Aditya Tiwari <tiwariadityasga3406@gmail.com>
2025-09-24 00:45:45 +0530

    Add files via upload

---
+++ Mad-Libs/mad libs.py
@@ -0,0 +1,15 @@
+#Mad libs game - Python project
+
+
+adjective_first = input("Enter the adjective: ")
+
+adjective_second = input("Enter the adjective: ")
+
+noun = input("Enter the noun: ")
+
+verb = input("Enter the verb: ")
+
+
+print(f"I went to a {adjective_first} zoo")
+print(f"I saw a {adjective_second} {noun}")
+print(f"It was {verb}ing")
